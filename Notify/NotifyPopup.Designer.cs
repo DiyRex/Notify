65f50eca9fa47a1283db41d85910f45b4a2e1baa
DiyRex <75021968+DiyRex@users.noreply.github.com>
2024-01-22 22:08:32 +0530

    Add files via upload

---
+++ Notify/NotifyPopup.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Notify
+{
+    partial class NotifyPopup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.notifyImageBox = new System.Windows.Forms.PictureBox();
+            this.lblMessageBody = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.notifyImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyImageBox
+            // 
+            this.notifyImageBox.BackColor = System.Drawing.Color.Transparent;
+            this.notifyImageBox.Location = new System.Drawing.Point(12, 12);
+            this.notifyImageBox.Name = "notifyImageBox";
+            this.notifyImageBox.Size = new System.Drawing.Size(80, 80);
+            this.notifyImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.notifyImageBox.TabIndex = 5;
+            this.notifyImageBox.TabStop = false;
+            // 
+            // lblMessageBody
+            // 
+            this.lblMessageBody.AutoSize = true;
+            this.lblMessageBody.BackColor = System.Drawing.Color.Transparent;
+            this.lblMessageBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessageBody.Location = new System.Drawing.Point(191, 47);
+            this.lblMessageBody.Name = "lblMessageBody";
+            this.lblMessageBody.Size = new System.Drawing.Size(11, 16);
+            this.lblMessageBody.TabIndex = 4;
+            this.lblMessageBody.Text = "-";
+            this.lblMessageBody.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(190, 12);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(16, 21);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "-";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // NotifyPopup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 104);
+            this.ControlBox = false;
+            this.Controls.Add(this.notifyImageBox);
+            this.Controls.Add(this.lblMessageBody);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotifyPopup";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            ((System.ComponentModel.ISupportInitialize)(this.notifyImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox notifyImageBox;
+        private System.Windows.Forms.Label lblMessageBody;
+        private System.Windows.Forms.Label lblTitle;
+    }
+}
---
+++ Notify/NotifyPopup.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Notify
+{
+    public partial class NotifyPopup : Form
+    {
+        private System.Windows.Forms.Timer animationTimer;
+        private int duration;
+        private int timeout;
+        public NotifyPopup(Color bodyColor, Color textColor, int duration, int timerInterval, double opacity, string title, string messageBody, string author = "", string notifyImage = null, int imgSize = 50)
+        {
+            InitializeComponent();
+            SetFormSize();
+            CenterFormOnScreen();
+
+            this.duration = duration;
+            this.timeout = timerInterval;
+
+            if (!bodyColor.IsEmpty)
+            {
+                this.BackColor = bodyColor;
+                this.lblMessageBody.ForeColor = textColor;
+                this.lblTitle.ForeColor = textColor;
+            }
+
+            arrangeContent(opacity, title, messageBody, author, notifyImage);
+
+            animationTimer = new System.Windows.Forms.Timer();
+            animationTimer.Interval = timerInterval;
+            animationTimer.Tick += aniamtionTimer_Tick;
+
+            animationTimer.Start();
+        }
+
+
+        private void arrangeContent(double opacity, string title, string messageBody, string author = "", string notifyImage = null, int imgSize = 50)
+        {
+            this.Opacity = opacity;
+
+
+            if (!string.IsNullOrEmpty(title) && title.Length > 30)
+            {
+                lblTitle.Text = title.Substring(0, 30);
+            }
+            else
+            {
+                lblTitle.Text = title;
+            }
+            if (!string.IsNullOrEmpty(messageBody) && messageBody.Length > 150)
+            {
+                lblMessageBody.Text = messageBody.Substring(0, 150);
+            }
+            else
+            {
+                lblMessageBody.Text = messageBody;
+            }
+            if (!string.IsNullOrEmpty(notifyImage))
+            {
+                Image image = Image.FromFile(notifyImage);
+
+                int newWidth = imgSize;
+                int newHeight = imgSize;
+
+                Bitmap resizedImage = new Bitmap(newWidth, newHeight);
+
+                using (Graphics g = Graphics.FromImage(resizedImage))
+                {
+                    g.DrawImage(image, 0, 0, newWidth, newHeight);
+                }
+
+                notifyImageBox.Image = resizedImage;
+            }
+            int title_width = (int)(this.Width - lblTitle.Width) / 2;
+            lblTitle.Location = new Point(title_width, lblTitle.Location.Y);
+            lblMessageBody.AutoSize = true;
+            lblMessageBody.TextAlign = ContentAlignment.MiddleLeft;
+            lblMessageBody.AutoEllipsis = false;
+
+            int labelWidth = (2 * this.Width) / 3;
+            lblMessageBody.Width = labelWidth;
+            lblMessageBody.MaximumSize = new Size(labelWidth, int.MaxValue);
+            lblMessageBody.Location = new Point((this.Width - lblMessageBody.Width + 50) / 2, lblTitle.Bottom + 15);
+
+
+
+        }
+        private void SetFormSize()
+        {
+            Screen screen = Screen.PrimaryScreen;
+            int width = (int)(screen.WorkingArea.Width * 1.5 / 5.0);
+            int height = (int)(screen.WorkingArea.Height * 0.5 / 5.0);
+
+            this.Size = new System.Drawing.Size(width, height);
+        }
+        private void CenterFormOnScreen()
+        {
+            Screen screen = Screen.PrimaryScreen;
+            int x = (screen.WorkingArea.Width - this.Width) / 2;
+            int y = -200;
+
+            this.Location = new System.Drawing.Point(x, y);
+        }
+        private void aniamtionTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.Location.Y < 0)
+            {
+                this.Location = new System.Drawing.Point(this.Location.X, this.Location.Y + 2);
+            }
+            else
+            {
+                animationTimer.Stop();
+                Thread.Sleep(duration);
+                rollUp();
+                animationTimer.Enabled = false;
+            }
+        }
+        private void rollUp()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.Location = new System.Drawing.Point(this.Location.X, this.Location.Y - 2);
+                Thread.Sleep(timeout);
+            }
+            this.Close();
+        }
+    }
+}
